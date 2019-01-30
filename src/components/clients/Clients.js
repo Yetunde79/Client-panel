55bb322e3e904ce6d51629019a1040d5b6747ecf
Yetunde Sola-Adebayo <ysolaadebayo@gmail.com>
2019-01-29 18:49:26 -0600

    make responsive

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -56,37 +56,39 @@ class Clients extends Component {
             </div>
           </div>
 
-          <table className="table table-stripe">
-            <thead className="thead-inverse">
-              <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Balance</th>
-                <th />
-              </tr>
-            </thead>
-
-            <tbody>
-              {clients.map(client => (
-                <tr key={client.id}>
-                  <td>
-                    {client.firstName} {client.lastName}
-                  </td>
-                  <td>{client.email}</td>
-                  <td>${parseFloat(client.balance).toFixed(2)}</td>
-                  <td>
-                    <Link
-                      to={`/client/${client.id}`}
-                      className="btn btn-secondary btn-sm"
-                    >
-                      <i className="fas fa arrow-circle-right" />
-                      Details
-                    </Link>
-                  </td>
+          <div className="hscroll">
+            <table className="table table-stripe">
+              <thead className="thead-inverse">
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Balance</th>
+                  <th />
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+
+              <tbody>
+                {clients.map(client => (
+                  <tr key={client.id}>
+                    <td>
+                      {client.firstName} {client.lastName}
+                    </td>
+                    <td>{client.email}</td>
+                    <td>${parseFloat(client.balance).toFixed(2)}</td>
+                    <td>
+                      <Link
+                        to={`/client/${client.id}`}
+                        className="btn btn-secondary btn-sm"
+                      >
+                        <i className="fas fa arrow-circle-right" />
+                        Details
+                      </Link>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       );
     } else {
